6ee6e26a69242ac081978eb716ef2a182ad50056
D.Coloma <dnl.coloma@gmail.coma>
2018-02-15 21:31:26 +0100

    fini

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,11 +2,9 @@ require 'uri'
 class MoviesController < ApplicationController
 
   def search
-
   end
 
   def result
-
     $results = SearchMovie.new(params[:movie][:search]).result
     new_uri
     redirect_to root_path
@@ -16,7 +14,7 @@ class MoviesController < ApplicationController
   def new_uri
     if !$results.nil?
       $results.each do |result|
-        result.poster_path = "https://image.tmdb.org/t/p/w500" + result.poster_path.to_s unless result.poster_path.nil?
+        result.poster_path = "https://image.tmdb.org/t/p/w500" + result.poster_path.to_s unless   result.poster_path.nil?
       end
     end
   end
